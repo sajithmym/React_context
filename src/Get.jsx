dfeb11d38fd5aaa82f6c66f4d552a560e5f143e7
Sajithmym <Sajithmym@gmail.com>
2023-05-15 16:15:16 +0530

    okay

--- src/Get.jsx
+++ src/Get.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react"
+import { AppContext } from "./App"
+
 // eslint-disable-next-line react/prop-types
-export default function Get({seting}) {
+export default function Get() {
+  const {set} = useContext(AppContext)
   return (
     <div>
         <input type='text' placeholder='Enter here' 
-        onChange={e => seting(e.target.value)}/>
+        onChange={e => set(e.target.value)}/>
     </div>
   )
 }
--- src/display.jsx
+++ src/display.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react"
+import { AppContext } from "./App"
+
 // eslint-disable-next-line react/prop-types
-export default function Display({show}) {
+export default function Display() {
+  const {name} = useContext(AppContext)
   return (
     <div>
-        <h1>Name is {show}</h1>
+        <h1>Name is {name}</h1>
         <img src="/s.jpg"/>
     </div>
   )
